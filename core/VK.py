041b06db6ae15813637f51ed1f1b4cb6c3803787
okbmsgnn <zeptonixx@gmail.com>
2021-01-21 12:39:03 +0200

    Fixed code bugs, added proxy

--- core/VK.py
+++ core/VK.py
@@ -1,26 +1,35 @@
 import requests
-import json
 import re
 
-class Context():
+
+class Context:
 
     def __init__(self, sendMessage, obj):
+        print(obj)
         self.message = obj.copy()
-        self.reply = lambda msg: sendMessage(obj["from_id"], msg)
-        
-class LongPoll():
-	
+        self.reply = lambda msg: sendMessage(self.message['peer_id'], msg)
+
+
+class LongPoll:
     commands = {}
 
     def __init__(self, token, group_id):
-		
+        #  PROXYREMOVE  #
+        self.ip = 'olegpogorili_mail_ru:5e60882a6a@45.13.192.215:30009'
+        self.proxies = {
+            'http': 'http://' + self.ip,
+            'https': 'http://' + self.ip
+        }
+        #  PROXYREMOVE END #
         self.key = None
         self.server = None
         self.ts = 0
         self.wait = 20
         
         self.session = requests.Session()
-        
+        #  PROXYREMOVE  #
+        self.session.proxies.update(self.proxies)
+        #  PROXYREMOVE END  #
         self.v = '5.80'
         self.token = token
         self.id = group_id
@@ -29,18 +38,17 @@ class LongPoll():
         
     def connect(self):
         
-        response = self.method('groups.getLongPollServer', {'group_id' : self.id})
+        response = self.method('groups.getLongPollServer', {'group_id': self.id})
         
         try:
             self.server = response['response']['server']
             self.key = response['response']['key']
             self.ts = response['response']['ts']
             print('[LongPoll] Connection TRUE!')
-        except: 
+        except:
             print(response)
             
     def check(self):
-		
         values = {
             'act': 'a_check',
             'key': self.key,
@@ -70,20 +78,20 @@ class LongPoll():
         for event in self.listen():
             if event['type'] == 'message_new':
                 text = event['object']['text']
-                matches = list(filter(lambda x: re.search(x, text) != None, list(self.commands.keys())))
+                matches = list(filter(lambda x: re.search(x, text) is not None, list(self.commands.keys())))
                 if len(matches) != 0:
                     self.commands[matches[0]](Context(self.sendMessage, event['object']))
 
-    def createContext(self, obj):
+    def create_context(self, obj):
         return {
             "reply": lambda msg: self.sendMessage(obj['peer_id'], msg),
             "message": obj.copy()
         }
 
     def command(self, regex, callback): 
-        callbackType = str(type(callback))
-        if 'function' not in callbackType:
-            raise Exception(f"Callback must be a function. Value: {callbackType}")
+        callback_type = str(type(callback))
+        if 'function' not in callback_type:
+            raise Exception(f"Callback must be a function. Value: {callback_type}")
 
         self.commands[regex] = callback
 
@@ -106,8 +114,8 @@ class LongPoll():
             
     def sendMessage(self, user_id, text):
         values = {
-            'user_id' : user_id,
-            'message' : text
+            'user_id': user_id,
+            'message': text
         }
         
-        self.method('messages.send', values) 
+        self.method('messages.send', values)
