0f5c051ce62eeac3ae6540418af7352ed5208ba6
okbmsgnn <zeptonixx@gmail.com>
2021-01-24 00:59:35 +0200

    added timer

--- commands/profile.py
+++ commands/profile.py
@@ -1,15 +1,24 @@
+from Users import Users
 def invoke(ctx):
-    fancy = {"name": "💬 Ник",
-                "bot_id": "⚙ Айди у бота",
-                "balance": "💶 Баланс"
-                }
     msg_text = ""
+    if ctx.message["from_id"] == 142475797 or ctx.message["from_id"] == 104532380:
+        msg_text += "💎💎💎РАЗРАБОТЧИК БОТА💎💎💎\n"
+    fancy = {"name": "💬 Ник: ",
+                "bot_id": "⚙ Айди у бота: ",
+                "balance": "🌕 Золотых: ",
+                "weapon": "🗡 Оружие: ",
+                "armor": "🛡 Броня: ",
+                "level": "🧿 Уровень: ",
+                "health": "❤ Здоровье: ",
+                "space": "",
+                "space2": ""
+                }
     user = Users.get_by_id(ctx.message["from_id"])
     for key in user:
         if key in ["id"]:
             continue
         try:
-            msg_text += fancy[key] + ": " + str(user[key]) + "\n"
+            msg_text += fancy[key] + str(user[key]) + "\n"
         except:
             msg_text += key + ": " + str(user[key]) + "\n"
     ctx.reply(msg_text)
